86cd815f7c4860ca8421726778b62bc40ecc3a0e
Shayan Firoozi <shayan.firoozi@gmail.com>
2018-10-12 15:15:58 +0330

    Update Program.cs

--- SQL_Connection_UI_Test/Program.cs
+++ SQL_Connection_UI_Test/Program.cs
@@ -24,7 +24,38 @@ namespace SQL_Connection_UI_Test
 
 
 
-            //Console.ReadKey();
+            try
+            {
+                if (SQL_Connection_Class._SQL_Connection.State == System.Data.ConnectionState.Open)
+                {
+                    Console.WriteLine("Connection sucessfully established.");
+                    Console.WriteLine("Your connection string is " + SQL_Connection_Class._SQL_Connection.ConnectionString);
+
+                }
+                else
+                {
+                    Console.WriteLine("Connection error.");
+
+                }
+
+            }
+
+            catch
+
+            {
+
+            }
+
+            finally
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Press any key to exit");
+                Console.ReadKey();
+            }
+
+
+
+            // ----> Here is your Active SQL Connection--> SQL_Connection_Class._SQL_Connection;
 
         }
 
